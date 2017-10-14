2a4e3974fff6b49331e274530f4527fdacd5d60b
Jopee Zapido <jopeezapido@users.noreply.github.com>
2017-10-14 16:45:09 +0800

    state problem in html and repeat solution

--- chunky-monkey-challenge.js
+++ chunky-monkey-challenge.js
@@ -1,32 +1,5 @@
-var lengthOfArray;
-var rem;
-var newArray = [];
-var bigArray = [];
-var slicer;
-var pushBig;
-
-function chunkArrayInGroups(arr, size) {
-  lengthOfArray = arr.length;
-  rem = lengthOfArray % size;
-
-    if (rem === 0){
-      //slicer = arr.slice(0,size)
-      for (i = 0; i <= arr.length; i++){
-        slicer = arr.slice(arr[i],size);
-        
-      }
-    }
-    console.log(slicer);
+function chunkArrayInGroups(arr,size) {
   
 }
 
-//Equal groupings:
-  chunkArrayInGroups(["a", "b", "c", "d"], 2);//[["a", "b"], ["c", "d"]]
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);//[[0, 1, 2], [3, 4, 5]]
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);//[[0, 1], [2, 3], [4, 5]]
-
-//Unequal groupings
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);//[[0, 1, 2, 3], [4, 5]]
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);//[[0, 1, 2], [3, 4, 5], [6]]
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);//[[0, 1, 2, 3], [4, 5, 6, 7], [8]]
-  //chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);//[[0, 1], [2, 3], [4, 5], [6, 7], [8]]
+chuckArrayInGroups(["a", "b", "c", "d"], 2);
